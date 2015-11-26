ed4ee00331422b7871152a31f3c72af1dab4be6d
Charles Sandidge <sandidgec@gmail.com>
2015-11-25 20:09:29 -0700

    added directories and files

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php require_once("php/head-utils.php"); ?>
+</head>
+<body class="site">
+    <div class="site-content">
+        <h2> GirlZilla Coming Soon!</h2>
+    </div>
+
+    <div class="emailform">
+        <?php require_once("php/emailform.php"); ?>
+    </div>
+
+</body>
+</html>
